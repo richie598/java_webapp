e20e8b31e05da0f7f5f4f8664765b4ef5a128683
Will Luongo <me@willluongo.com>
2013-08-26 17:34:20 -0700

    Added JNDI Datasource example

--- src/com/engineyard/javademo/HelloJNDI.java
+++ src/com/engineyard/javademo/HelloJNDI.java
@@ -1,6 +1,19 @@
 package com.engineyard.javademo;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 public class HelloJNDI extends HttpServlet {
 
@@ -8,5 +21,56 @@ public class HelloJNDI extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 3349334178242798897L;
+	private Context ctx = null;
+	private DataSource ds = null;
+	private String message = null;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			initializeDB();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			message = e.toString();
+		}
+		if (message == null)
+			getMessage("jndi");
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println(message);
+		response.getWriter().println(
+				"session=".concat(request.getSession(true).getId()));
+	}
+
+	private void initializeDB() throws NamingException {
+		ctx = new InitialContext();
+		ds = (DataSource)ctx.lookup("java:comp/env/jdbc/DSTest");
+	}
+
+	private void getMessage(String string) {
+		Connection con = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			con = ds.getConnection();
+			statement = con.createStatement();
+			resultSet = statement
+					.executeQuery("select message from javademo.javademo where id = 'jndi'");
+			while (resultSet.next()) {
+				message = resultSet.getString(1);
+			}
+
+		} catch (SQLException e) {
+			message = "Query failed: " + e;
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					// I DON'T CARE
+				}
+		}
+	}
 
 }
